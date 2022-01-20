aea379687b34255b32820930d56429a4312fb657
Matthias Poortvliet <matthiaspoortvliet@gmail.com>
2022-01-20 13:44:25 +0100

    updated deploy methods

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,7 @@ import { defineNuxtConfig } from 'nuxt3'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   serverMiddleware: [
-    { path: '/api', handler: '~/server/api/index.ts' },
-    { path: '/favicon.ico', handler: '~/public/favicon.ico' }
+    { path: '/api', handler: '~/server/api/index.ts' }
   ],
   meta: {
     meta: [
