8904e85da09b4e678acc1eef25d7f44909e98492
Matthias Poortvliet <matthiaspoortvliet@gmail.com>
2022-01-20 16:22:19 +0100

    added env for db

--- server/api/db/index.ts
+++ server/api/db/index.ts
@@ -4,7 +4,7 @@ let client
 
 export default async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/db')
+    await mongoose.connect('mongodb://db:722a35408a65551e8abcd96f94f4d544@dokku-mongo-db:27017/db')
     client = mongoose.connection
   } catch (error) {
     console.log(error)
