494964a829d26693988efc6a5643e4ab2281695d
Matthias Poortvliet <matthiaspoortvliet@gmail.com>
2022-05-31 16:18:02 +0200

    bugfix in var names env

--- plugins/urql.ts
+++ plugins/urql.ts
@@ -2,9 +2,9 @@ import { env } from 'process';
 import { defineNuxtPlugin } from '#app'
 import urql from '@urql/vue'
 
-const port = process.env.DOKKU_PROXY_PORT || 4000
-const url = process.env.MONGO_URL_SUFFIX || 'gql'
-const host = process.env.HOSTNAME || 'localhost'
+const port = env.DOKKU_PROXY_PORT || 4000
+const url = env.MONGO_URL_SUFFIX || 'gql'
+const host = env.HOSTNAME || 'localhost'
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(urql, {
