04b039d0958c0248dfa3d8ad2b07fda7197bc04c
Matthias Poortvliet <matthiaspoortvliet@gmail.com>
2022-05-31 16:56:43 +0200

    updated urql setup

--- plugins/urql.ts
+++ plugins/urql.ts
@@ -1,13 +1,13 @@
-import { env } from 'process';
 import { defineNuxtPlugin } from '#app'
 import urql from '@urql/vue'
 
-const port = env.DOKKU_PROXY_PORT || 4000
-const url = env.MONGO_URL_SUFFIX || 'gql'
-const host = env.HOSTNAME || 'localhost'
+const port = 4000
+const url = 'gql'
+const host = 'localhost'
+const graphQlUrl = `http://${host}:${port}/${url}`
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(urql, {
-    url: `http://${host}:${port}/${url}`
+    url: graphQlUrl
   })
 })
