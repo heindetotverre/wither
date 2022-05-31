e5de26c34f12a8b44e3b8cc4c3000c13df64fe48
Matthias Poortvliet <matthiaspoortvliet@gmail.com>
2022-05-31 21:27:28 +0200

    u[dated graphql client url

--- plugins/urql.ts
+++ plugins/urql.ts
@@ -1,10 +1,11 @@
 import { defineNuxtPlugin } from '#app'
 import urql from '@urql/vue'
 
-const port = 4000
-const url = 'gql'
-const host = 'localhost'
-const graphQlUrl = `http://${host}:${port}/${url}`
+// const port = 4000
+// const url = 'gql'
+// const host = 'localhost'
+// const graphQlUrl = `http://${host}:${port}/${url}`
+const graphQlUrl = 'http:42183:5000'
 
 console.log(`===> graphql client url: ${graphQlUrl} <===`)
 
