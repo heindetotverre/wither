fdb612d25c4e210291aa0ba393f1a88f18514fd9
Matthias Poortvliet <matthiaspoortvliet@gmail.com>
2022-05-31 22:17:30 +0200

    test

--- plugins/urql.ts
+++ plugins/urql.ts
@@ -1,13 +1,7 @@
 import { defineNuxtPlugin } from '#app'
 import urql from '@urql/vue'
 
-// const port = 4000
-// const url = 'gql'
-// const host = 'localhost'
-// const graphQlUrl = `http://${host}:${port}/${url}`
-const graphQlUrl = 'http:42183:5000'
-
-console.log(`===> graphql client url: ${graphQlUrl} <===`)
+const graphQlUrl = `http://server.heindetotverre.com/gql`
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(urql, {
--- server/auth/mongoConnect.ts
+++ server/auth/mongoConnect.ts
@@ -1,9 +1,11 @@
 import { env } from 'process';
 import mongoose from 'mongoose'
 
+const mongoUrl = env.MONGO_URL || 'mongodb://127.0.0.1:27017/db' as string
+
 export default async () => {
   try {
-    await mongoose.connect(env.MONGO_URL as string)
+    await mongoose.connect(mongoUrl)
   } catch (error) {
     console.log(error)
     return
