f4ff0518e5a79da2fba84f4c2ffd148c26df17ac
Matthias Poortvliet <matthiaspoortvliet@gmail.com>
2022-05-31 21:20:48 +0200

    added connect log

--- plugins/urql.ts
+++ plugins/urql.ts
@@ -6,6 +6,8 @@ const url = 'gql'
 const host = 'localhost'
 const graphQlUrl = `http://${host}:${port}/${url}`
 
+console.log(`===> graphql client url: ${graphQlUrl} <===`)
+
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(urql, {
     url: graphQlUrl
