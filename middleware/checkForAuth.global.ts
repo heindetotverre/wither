8fe60797ed05d75ef919c82ae02aa2d14a2e1cce
Matthias <matthias.poortvliet@kpn.com>
2022-06-02 22:22:41 +0200

    auth fixes, only cookie now

--- middleware/checkForAuth.global.ts
+++ middleware/checkForAuth.global.ts
@@ -1,50 +1,52 @@
-import { Errors } from '~~/types/enums'
+// import { Errors } from '~~/types/enums'
 
-let redirect : boolean
+// const fetchToken = async (tokenId : string) => {
+//   try {
+//     const result = await GqlFetchToken({tokenId: tokenId})
+//     return result
+//   } catch (e) {
+//     console.log(`${Errors.DB_ERROR_GET_TOKEN}: ${e}`)
+//   }
+// }
 
-const fetchToken = async (tokenId : string) => {
-  try {
-    const result = await GqlFetchToken({tokenId: tokenId})
-    return result
-  } catch (e) {
-    console.log(`${Errors.DB_ERROR_GET_TOKEN}: ${e}`)
-  }
-}
+// const deleteToken = async (tokenId : string) => {
+//   try {
+//     await GqlDeleteToken({tokenId: tokenId})
+//   } catch (e) {
+//     console.log(`${Errors.DB_ERROR_DELETE_TOKEN}: ${e}`)
+//   }
+// }
 
-const deleteToken = async (tokenId : string) => {
-  try {
-    await GqlDeleteToken({tokenId: tokenId})
-  } catch (e) {
-    console.log(`${Errors.DB_ERROR_DELETE_TOKEN}: ${e}`)
-  }
-}
-
-export default defineNuxtRouteMiddleware(async (to, from) => {
+export default defineNuxtRouteMiddleware(async (to) => {
   const { value } = useCookie('witherLoginToken') as any,
     tokenId = value?.id
-
   if (tokenId) {
-    const tokenMatchFromDb = await fetchToken(tokenId)
-    if (!tokenMatchFromDb) {
+    // const tokenMatchFromDb = await fetchToken(tokenId)
+    // if (!tokenMatchFromDb) {
+    //   useCookie('witherLoginToken', { path: '/' , maxAge: 0})
+    //   return navigateTo('/admin/login')
+    // } else {
+    //   const now = new Date()
+    //   const twoHours = 10800000
+    //   if (tokenMatchFromDb.getToken?.created as number < (now.getTime() - twoHours)) {
+    //     await deleteToken(tokenId)
+    //     useCookie('witherLoginToken', { path: '/' , maxAge: 0})
+    //     return navigateTo('/admin/login')
+    //   } else {
+    //     if (to.fullPath.includes('admin/login')) {
+    //       return navigateTo('/admin')
+    //     }
+    //   }
+    // }
+    const now = new Date()
+    const twoHours = 10800000
+    if (value.created as number < (now.getTime() - twoHours)) {
       useCookie('witherLoginToken', { path: '/' , maxAge: 0})
-      redirect = true
-    } else {
-      const now = new Date()
-      const twoHours = 10800000
-      if (tokenMatchFromDb.getToken?.created as number < (now.getTime() - twoHours)) {
-        await deleteToken(tokenId)
-        useCookie('witherLoginToken', { path: '/' , maxAge: 0})
-        redirect = true
-      } else {
-        if (to.fullPath.includes('admin/login')) {
-          return navigateTo('/admin')
-        }
-      }
+      return navigateTo('/admin/login')
     }
   } else {
-    redirect = true
-  }
-  if (redirect && to.fullPath.includes('admin') && to.fullPath !== '/admin/login') {
-    return navigateTo('/admin/login')
+    if (to.fullPath.includes('admin') && to.fullPath !== '/admin/login') {
+      return navigateTo('/admin/login')
+    }
   }
 })
