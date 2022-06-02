a54dd66fbcf7fa659d2754ac387648869a498f88
Matthias <matthias.poortvliet@kpn.com>
2022-06-02 20:30:43 +0200

    component order refactor

--- pages/admin/login/index.vue
+++ pages/admin/login/index.vue
@@ -1,3 +1,36 @@
+<script setup lang="ts">
+  import { authStore } from '~~/store/auth'
+  import { formStore } from '~~/store/forms'
+  import { User } from '~~/types/types'
+  import { Auth, FormNames } from '~~/types/enums'
+
+  const response = ref(),
+    formRenderer = ref(Auth.Login),
+    loginForm = ref(formStore.get.getLoginForm()),
+    registerForm = ref(formStore.get.getRegisterForm())
+
+
+  const auth = async (method: Auth, event: User) => {
+    const authResult = await authStore.do[method](event) as any
+    if (!authResult?.response?.errors) {
+      useRouter().push('/')
+    } else {
+      if (method === Auth.Login && authResult?.response?.errors.find((error : any) => error.message.includes('User not found'))) {
+        formStore.do.updateAllFormValues(FormNames.LOGIN, 'clear')
+      }
+    }
+    response.value = authResult
+  }
+
+  const switchAuthMethod = () => {
+    formRenderer.value = formRenderer.value === Auth.Login
+      ? Auth.Register
+      : Auth.Login
+
+    response.value = ''
+  }
+</script>
+
 <template>
   <div>
     <p>Login section</p>
@@ -14,35 +47,3 @@
     <div v-if="response">{{ response.error ? response.error.message : response }}</div>
   </div>
 </template>
-<script setup lang="ts">
-import { authStore } from '~~/store/auth'
-import { formStore } from '~~/store/forms'
-import { User } from '~~/types/types'
-import { Auth, FormNames } from '~~/types/enums'
-
-const response = ref(),
-  formRenderer = ref(Auth.Login),
-  loginForm = ref(formStore.get.getLoginForm()),
-  registerForm = ref(formStore.get.getRegisterForm())
-
-
-const auth = async (method: Auth, event: User) => {
-  const authResult = await authStore.do[method](event) as any
-  if (!authResult?.response?.errors) {
-    useRouter().push('/')
-  } else {
-    if (method === Auth.Login && authResult?.response?.errors.find((error : any) => error.message.includes('User not found'))) {
-      formStore.do.updateAllFormValues(FormNames.LOGIN, 'clear')
-    }
-  }
-  response.value = authResult
-}
-
-const switchAuthMethod = () => {
-  formRenderer.value = formRenderer.value === Auth.Login
-    ? Auth.Register
-    : Auth.Login
-
-  response.value = ''
-}
-</script>
