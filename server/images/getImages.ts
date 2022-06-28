370d053b29d038d56371355c0b45bd946e8de0ac
Matthias Poortvliet <matthiaspoortvliet@gmail.com>
2022-06-28 13:44:02 +0200

    updated images path based on env

--- server/images/getImages.ts
+++ server/images/getImages.ts
@@ -2,9 +2,11 @@ import { fileURLToPath } from 'url';
 import * as fs from 'fs'
 import path from 'path'
 
+const env = process.env.NODE_ENV
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const imageFolder = path.join(__dirname, '../../public/images');
+const imageFolder = path.join(__dirname, env === 'development' ? '../../public/images' : '../../images');
 
 export default async () => {
   const files = fs.readdirSync(imageFolder, {withFileTypes: true})
